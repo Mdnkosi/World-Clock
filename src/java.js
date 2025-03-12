3597ef4e5639f452c35060825ea3b44b65c8b213
Mdnkosi <mahlatsedineo93@gmail.com>
2025-03-12 18:15:04 +0200

    Added a link to the home page and some styling.

--- src/java.js
+++ src/java.js
@@ -19,31 +19,40 @@ function updateTime() {
   johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
   johannesburgTimeElement.innerHTML = johannesburgTime.format(
     "h:mm:ss [<small>]A[<small/]>"
-  );
+    );
+    
+   
+  }
+ 
 }
-}
 
-
-
-
-function updateCity(event) {
-  let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
+ function updateCity(event) {
+   let cityTimeZone = event.target.value;
+   if (cityTimeZone === "current") {
+     cityTimeZone = moment.tz.guess();
    }
 
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
+   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+   let cityTime = moment().tz(cityTimeZone);
+   let citiesElement = document.querySelector("#cities");
+   citiesElement.innerHTML = `
   
   <div class="city">
   <div>
     <h2>${cityName}</h2>
     <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
   </div>
-  <div class="time">${cityTime.format("h:mm:ss ")} <small>${cityTime.format("A")}</small></div>`;
-}
+  <div class="time">${cityTime.format("h:mm:ss ")} <small>${cityTime.format(
+     "A"
+   )}</small></div>
+   </div>
+     <a href="index.html">All Cities <a/>`;
+     
+   
+ }
+
+
+
 updateTime();
 setInterval(updateTime, 1000);
 
